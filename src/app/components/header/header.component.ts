18cde8ed3b2e37efb297b1e0e5ffa88e92104652
Hector Giraldo <hectorgiraldo0210@gmail.com>
2025-02-25 13:14:03 +0100

    modificacion header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,21 @@
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: "app-header",
+  selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
-  templateUrl: "./header.component.html",
-  styleUrl: "./header.component.css",
+  imports: [RouterModule, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+  closeMenu() {
+    this.menuOpen = false;
+  }
+}
