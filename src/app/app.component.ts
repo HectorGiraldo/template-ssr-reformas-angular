1dcc4f5a61eba544dbad8772fc02b7e8e1892ee6
Hector Giraldo <hectorgiraldo0210@gmail.com>
2025-02-25 11:09:53 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { DataService } from './data.service';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'servicios',
+    loadComponent: () =>
+      import('./pages/servicios/servicios.component').then(
+        (m) => m.ServiciosComponent
+      ),
+  },
+  {
+    path: 'contacto',
+    loadComponent: () =>
+      import('./pages/contacto/contacto.component').then(
+        (m) => m.ContactoComponent
+      ),
+  },
+  {
+    path: 'blog',
+    loadComponent: () =>
+      import('./pages/blog/blog.component').then((m) => m.BlogComponent),
+  },
+  { path: '**', redirectTo: '' },
+];
