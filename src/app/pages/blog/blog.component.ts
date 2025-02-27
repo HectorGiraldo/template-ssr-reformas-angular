a05f4e3e189de8931142476bc350223e3c965237
Hector Giraldo <hectorgiraldo0210@gmail.com>
2025-02-27 14:03:00 +0100

    estructura blog

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-blog',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './blog.component.html',
-  styleUrl: './blog.component.css'
+  styleUrl: './blog.component.css',
 })
 export class BlogComponent {
-
+  items = [1, 2, 3, 4, 5, 6];
 }
