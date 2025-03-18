b7048b1d50e3449031b365e1d8a731f19477921c
Hector Giraldo <hectorgiraldo0210@gmail.com>
2025-03-18 14:13:34 +0100

    Politicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,5 +25,26 @@ export const routes: Routes = [
   //   loadComponent: () =>
   //     import('./pages/blog/blog.component').then((m) => m.BlogComponent),
   // },
+  {
+    path: 'aviso-legal',
+    loadComponent: () =>
+      import('./pages/aviso-legal/aviso-legal.component').then(
+        (m) => m.AvisoLegalComponent
+      ),
+  },
+  {
+    path: 'politica-privacidad',
+    loadComponent: () =>
+      import('./pages/politica-privacidad/politica-privacidad.component').then(
+        (m) => m.PoliticaPrivacidadComponent
+      ),
+  },
+  {
+    path: 'politica-cookies',
+    loadComponent: () =>
+      import('./pages/politica-cookies/politica-cookies.component').then(
+        (m) => m.PoliticaCookiesComponent
+      ),
+  },
   { path: '**', redirectTo: '' },
 ];
