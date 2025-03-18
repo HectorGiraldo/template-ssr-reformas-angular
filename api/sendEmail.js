6fe811dae43a7af3d0898d5b4dc4a332d42bc8eb
Hector Giraldo <hectorgiraldo0210@gmail.com>
2025-03-18 14:57:02 +0100

    contacto formulario

--- api/sendEmail.js
+++ api/sendEmail.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { name, email, message } = req.body;
+  const { name, email, message, phone } = req.body;
 
   // Configuración del transportador SMTP con OVH
   const transporter = nodemailer.createTransport({
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
       from: `"${name}" <${process.env.EMAIL_USER}>`, // Nombre + correo que envía
       to: process.env.EMAIL_RECEIVER, // Destinatario
       subject: `Mensaje de ${name}`,
-      text: message,
+      text: { message, phone },
       replyTo: email, // Para que el destinatario pueda responder al remitente
     };
 
--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,23 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { EmailService } from '../../services/email.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contacto',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './contacto.component.html',
   styleUrl: './contacto.component.css',
 })
-export class ContactoComponent {
+export class ContactoComponent implements OnInit {
+  contacForm!: FormGroup;
   constructor(private email: EmailService) {}
 
+  ngOnInit() {
+    this.initForm();
+  }
+
   sendEmail() {
-    this.email
-      .sendEmail({
-        name: 'hector',
-        email: 'boykan30@gmail.com',
-        message: 'Hello World correo funciona',
-      })
-      .subscribe(console.log);
+    this.email.sendEmail(this.contacForm.value).subscribe(console.log);
+  }
+
+  initForm() {
+    this.contacForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+      policy: new FormControl(false, Validators.requiredTrue),
+    });
   }
 }
